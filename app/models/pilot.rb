3628a94a19510ae50b9e3d4bd7fdf15ec63ec2d0
Joe Seiden <joe.seiden214@gmail.com>
2017-09-20 15:20:57 -0700

    Fix syntax issue with belongs_to relation on pilot

--- app/models/pilot.rb
+++ app/models/pilot.rb
@@ -2,8 +2,8 @@ class Pilot < ApplicationRecord
   validates :callsign, :pilot_skill, :player, :campaign, presence: true
 
   belongs_to :player,
-    class_name :User,
-    foreign_key :user_id
+    class_name: :User,
+    foreign_key: :user_id
 
   belongs_to :campaign
 end
